43a3fc60c6311de3f41db8417ccb638ee121c4f9
Mahmud hasan <mhfahad93@gmail.com>
2019-05-21 10:13:25 +0600

    first001

---
+++ Hello/Hello/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Hello
+{
+    partial class HelloApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonShow
+            // 
+            this.ButtonShow.Location = new System.Drawing.Point(386, 198);
+            this.ButtonShow.Name = "ButtonShow";
+            this.ButtonShow.Size = new System.Drawing.Size(75, 23);
+            this.ButtonShow.TabIndex = 0;
+            this.ButtonShow.Text = "Show";
+            this.ButtonShow.UseVisualStyleBackColor = true;
+            this.ButtonShow.Click += new System.EventHandler(this.ButtonShow_Click);
+            // 
+            // HelloApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonShow);
+            this.Name = "HelloApp";
+            this.Text = "Hello";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonShow;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonShow
+            // 
+            this.ButtonShow.Location = new System.Drawing.Point(354, 185);
+            this.ButtonShow.Name = "ButtonShow";
+            this.ButtonShow.Size = new System.Drawing.Size(75, 23);
+            this.ButtonShow.TabIndex = 0;
+            this.ButtonShow.Text = "Show";
+            this.ButtonShow.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 417);
+            this.Controls.Add(this.ButtonShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonShow;
+    }
+}
+
